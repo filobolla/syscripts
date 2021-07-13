527ee1b29e8292d14dead6a2a14317dc5749fd8d
Filippo Bollarino <filo.bolla@htomail.com>
2021-07-13 20:50:41 +0200

    Cancelled BOT_TOKEN to avoid exfiltration

--- tgram_notify/ovpn_tgram_notify_wget.sh
+++ tgram_notify/ovpn_tgram_notify_wget.sh
@@ -2,9 +2,9 @@
 # ovpn_tgram_notify: telegram notification handler for OpenVPN-related events
 # wget can be replaced by curl to unlock different content-type encodings (application/json)
 PGRM=$1
-CHAT_ID="657316162"
+CHAT_ID="<CHAT_ID>"
 HOST=$(hostname)
 HEADER="<b>${PGRM}</b> (${HOST%%.*})says:%0a"
 DESCRIPTION="Connection from <u>${trusted_ip}</u>to <u>${HOST%%.*}</u> established $(echo -e '\xE2\x9C\x85')%0aClient: <i>${common_name}</i>%0aTunnel IP: <u>${ifconfig_pool_remote_ip}</u>"
 message="${HEADER}${DESCRIPTION}"
-wget --post-data 'chat_id='${CHAT_ID}'&parse_mode=HTML&text='"${message}"'' https://api.telegram.org/bot813234765:AAFEiAazebcro07zgsL9Gnb7DOvvpMr_oiw/sendMessage >/dev/null 2> /var/log/openvpn/ovpn_tgram_notify.log
+wget --post-data 'chat_id='${CHAT_ID}'&parse_mode=HTML&text='"${message}"'' https://api.telegram.org/bot<BOT_TOKEN>/sendMessage >/dev/null 2> /var/log/openvpn/ovpn_tgram_notify.log
--- tgram_notify/ups_tgram_notify.sh
+++ tgram_notify/ups_tgram_notify.sh
@@ -4,12 +4,12 @@
 # Message can be manually written inside the script if this isn't a daemon
 PGRM=$1
 LOG="/var/log/ups/ups_tgram_notify.log"
-CHAT_ID="657316162"
+CHAT_ID="<CHAT_ID>"
 HOST=$(hostname)
 HEADER="<b>${PGRM}</b> (${HOST%%.*}) says:\n"
 DESCRIPTION=$(echo -e $2)
 message="${HEADER}${DESCRIPTION}"
-curl -X POST -H 'Content-Type: application/json' -d '{"chat_id":"'${CHAT_ID}'", "parse_mode":"HTML", "text":"'"${message}"'"}' https://api.telegram.org/bot1787054821:AAFQAmbtdSpaYYIzFNtXBp8izpCjtkQewn0/sendMessage >/dev/null 2> $LOG
+curl -X POST -H 'Content-Type: application/json' -d '{"chat_id":"'${CHAT_ID}'", "parse_mode":"HTML", "text":"'"${message}"'"}' https://api.telegram.org/bot<BOT_TOKEN>/sendMessage >/dev/null 2> $LOG
 # If wget is used, special characters must be formatted in urlencoded format (eg. line breaks)
 #message=${HEADER/'\n'/'%0a'}${DESCRIPTION/'\n'/'%0a'}
 #wget --post-data 'chat_id='${CHAT_ID}'&parse_mode=HTML&text='"${message}"'' https://api.telegram.org/bot813234765:AAFEiAazebcro07zgsL9Gnb7DOvvpMr_oiw/sendMessage >/dev/null 2>> $LOG
